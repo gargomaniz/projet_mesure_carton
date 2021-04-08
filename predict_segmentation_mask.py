fd284d400cd3f9bc914286fa8857dcd68d609cb9
ThibaultPro <67201433+ThibaultPro@users.noreply.github.com>
2021-04-08 20:14:26 +0200

    Add files via upload

--- predict_segmentation_mask.py
+++ predict_segmentation_mask.py
@@ -10,19 +10,16 @@ from numpy import asarray
 
 from PIL import Image
 
-try:
-  # %tensorflow_version only exists in Colab.
-#   %tensorflow_version 2.x
-except Exception:
-  pass
 
 import tensorflow as tf
 from matplotlib import pyplot as plt
-import tensorflow_datasets as tfds
+#import tensorflow_datasets as tfds
 import seaborn as sns
 
 import time
 import PIL
+
+from process_masks import mask_processor, load_from_opencv_xml
 
 print("Tensorflow version " + tf.__version__)
 
@@ -60,19 +57,19 @@ def give_color_to_annotation(annotation):
 
 """### **Load the model**"""
 
-reconstructed_model = tf.keras.models.load_model("./unet_model5_170epochs.h5") #Path to the model
+reconstructed_model = tf.keras.models.load_model("./unet_model.h5") #Path to the model
 
 """### **Image to predict**"""
 
 #load image to predict and resize to shape (128, 128, 3)
-image_path = './carton.jpg' #Image that we wabt to predict segmentation mask
-
+image_path1 = './carton_7_pos1.jpg' #Image that we wabt to predict segmentation mask
+image_path2 = './carton_7_pos2.jpg'
 """### **Predict segmentation mask**"""
 
 height=128
 width=128
 
-img_raw = tf.io.read_file(image_path)
+img_raw = tf.io.read_file(image_path1)
 image = tf.image.decode_jpeg(img_raw)
 
 image = tf.image.resize(image, (height, width))
@@ -92,22 +89,58 @@ prediction= np.argmax(prediction, axis=3)
 pred_img = give_color_to_annotation(prediction[0])
 
 image = image*255
-images = np.uint8([image, pred_img])
+images1 = np.uint8([image, pred_img])
 
 
+#passing image from camera 2 to the Network
+
+img_raw = tf.io.read_file(image_path2)
+image = tf.image.decode_jpeg(img_raw)
+
+image = tf.image.resize(image, (height, width))
+image = tf.reshape(image, (height, width, 3,))
+image = tf.cast(image, dtype=tf.float32) /255.0
+
+#resize shape because UNet input is (None, 128,128,3)
+images = tf.reshape(image, (1,height, width, 3,))
+
+#prediction of the image and reformating output from (128,128,2) to (128,128,1)
+prediction = reconstructed_model.predict(images)
+
+prediction= np.argmax(prediction, axis=3)
+
+#changing values of output from binary to colorized image for display
+pred_img = give_color_to_annotation(prediction[0])
+
+image = image*255
+images2 = np.uint8([image, pred_img])
+
+images = images1 + images2
 #displaying image with predicted mask
 plt.figure(figsize=(10,4))
 
-for idx, im in enumerate(images):
-  plt.subplot(1, 2, idx+1)
+for idx, im in enumerate(images2):
+  plt.subplot(2, 2, idx+1)
   plt.xticks([])
   plt.yticks([])
   #plt.title(titles[idx], fontsize=12)
   plt.imshow(im)
+  plt.show()
 
+H12 = load_from_opencv_xml("homography2to1.xml", "H")
+
+Hv1_1 = load_from_opencv_xml("Hv1_1.xml", "Hv1_1")
+Hv1_2 = load_from_opencv_xml("Hv1_2.xml", "Hv1_2")
+
+lines, h, l = mask_processor(images1[0], images1[1], images2[0], images2[1], H12, Hv1_1, Hv1_2)
+
+print("h", h)
+print("l", l)
+plt.imshow(lines)
+plt.show()
 print(type(im))
 
-new_im = Image.fromarray(im)
+new_im = Image.fromarray(lines)
 
 """### **Save the segmentation mask**"""
 
